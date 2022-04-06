d3e096f089308dfde7c276edd8a61806b7c99ef9
Yeray Candel <ycansam@epsg.upv.es>
2022-04-06 19:22:33 +0200

    Added Estructure

--- build/turtlebot3_fake_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yeray/Documentos/GitHub/Ysolve-bot/src/turtlebot3_simulations/turtlebot3_fake_node
-# Build directory: /home/yeray/Documentos/GitHub/Ysolve-bot/build/turtlebot3_fake_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ysolve_bot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yeray/Documentos/GitHub/Ysolve-bot/src/ysolve_bot/ysolve_bot
-# Build directory: /home/yeray/Documentos/GitHub/Ysolve-bot/build/ysolve_bot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
