9ecb2010500740ab10e21a0950e3e5b65fe5159b
Yeray Candel <ycansam@epsg.upv.es>
2022-04-06 19:07:36 +0200

    Structure Part 1

---
+++ build/ysolve_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yeray/Documentos/GitHub/Ysolve-bot/src/ysolve_bot/ysolve_bot
+# Build directory: /home/yeray/Documentos/GitHub/Ysolve-bot/build/ysolve_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
