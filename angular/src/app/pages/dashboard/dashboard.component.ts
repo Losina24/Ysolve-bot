f44354b318a3b59338ea45f44ccf9aabbacdd3f4
Alejandro <losina.dd@gmail.com>
2022-04-09 23:17:02 +0200

    Extra menus added

--- angular/src/app/pages/dashboard/dashboard.component.ts
+++ angular/src/app/pages/dashboard/dashboard.component.ts
@@ -9,6 +9,10 @@ export class DashboardComponent implements OnInit {
 
   navigation: Navigation = Navigation.MANUAL;
   Navigation: typeof Navigation = Navigation;
+  ManualNavigation: typeof ManualNavigation = ManualNavigation;
+  AutomaticNavigation: typeof AutomaticNavigation = AutomaticNavigation;
+  robotMenu: boolean = false;
+  cameraMenu: boolean = false;
 
   constructor() { }
 
@@ -22,9 +26,44 @@ export class DashboardComponent implements OnInit {
       this.navigation = Navigation.AUTOMATIC;
     }
   }
+
+  manualNavigationAction(action: ManualNavigation) {
+
+  }
+
+  automaticNavigationAction(action: AutomaticNavigation) {
+
+  }
+
+  toggleRobotMenu(status: boolean) {
+    this.cameraMenu = false;
+    this.robotMenu = status;
+  }
+
+  toggleCameraMenu(status: boolean) {
+    this.robotMenu = false;
+    this.cameraMenu = status;
+  }
 }
 
 enum Navigation {
   MANUAL,
   AUTOMATIC
+}
+
+enum ManualNavigation {
+  FORWARD = "Hacia delante",
+  BACK = "Hacia atrás",
+  LEFT = "Izquierda",
+  RIGHT = "Derecha",
+  STOP = "Parar"
+}
+
+enum AutomaticNavigation {
+  POINT_A = "Punto A",
+  POINT_B = "Punto B",
+  POINT_C = "Punto C",
+  POINT_D = "Punto D",
+  POINT_E = "Punto E",
+  POINT_F = "Punto F"
 }
