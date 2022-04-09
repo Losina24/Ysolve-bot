31e6d88e09cc21da0d8597089708ced104a52115
Alejandro <losina.dd@gmail.com>
2022-04-10 00:08:59 +0200

    Add animations

--- angular/src/app/pages/dashboard/dashboard.component.ts
+++ angular/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//@ts-ignore
+import anime from 'animejs';
 
 @Component({
   selector: 'app-dashboard',
@@ -17,6 +19,7 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.move([0,0], [0, -200])
   }
 
   changeNavigation() {
@@ -43,6 +46,19 @@ export class DashboardComponent implements OnInit {
   toggleCameraMenu(status: boolean) {
     this.robotMenu = false;
     this.cameraMenu = status;
+  }
+
+  move(x: any, y: any) {
+    anime({
+      targets: '.rbs',
+      translateX: [x[0], x[1]],
+      translateY: [y[0], y[1]],
+      opacity: .2,
+      delay: 500,
+      duration: 2000,
+      easing: 'easeInOutExpo',
+      loop: true
+    });
   }
 }
 
