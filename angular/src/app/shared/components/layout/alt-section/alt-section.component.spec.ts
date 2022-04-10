183bac3d11784c448e2e2f0b47a6f08a1ca3d0b8
Alejandro <losina.dd@gmail.com>
2022-04-11 00:50:13 +0200

    Merge branches

---
+++ angular/src/app/shared/components/layout/alt-section/alt-section.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AltSectionComponent } from './alt-section.component';
+
+describe('AltSectionComponent', () => {
+  let component: AltSectionComponent;
+  let fixture: ComponentFixture<AltSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AltSectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AltSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular/src/app/shared/components/layout/smartphone-render/smartphone-render.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SmartphoneRenderComponent } from './smartphone-render.component';
+
+describe('SmartphoneRenderComponent', () => {
+  let component: SmartphoneRenderComponent;
+  let fixture: ComponentFixture<SmartphoneRenderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SmartphoneRenderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SmartphoneRenderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
